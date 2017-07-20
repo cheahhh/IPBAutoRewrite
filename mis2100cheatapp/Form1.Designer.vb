633d41caec966453cea87512090e8a4f681f68bf
cheahhh <j15017367@student.newinti.edu.my>
2017-07-20 22:18:02 +0800

    Add files via upload

---
+++ mis2100cheatapp/Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton8 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ForumMainPageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UserProfileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SeeUsersPostToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SeeUsersTopicsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripButton9 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton5 = New System.Windows.Forms.ToolStripButton()
+        Me.tsp = New System.Windows.Forms.ToolStripProgressBar()
+        Me.ToolStripButton7 = New System.Windows.Forms.ToolStripButton()
+        Me.tm = New System.Windows.Forms.Timer(Me.components)
+        Me.tm2 = New System.Windows.Forms.Timer(Me.components)
+        Me.lb1 = New System.Windows.Forms.ListBox()
+        Me.ToolStrip2 = New System.Windows.Forms.ToolStrip()
+        Me.stat = New System.Windows.Forms.ToolStripLabel()
+        Me.l_ref = New System.Windows.Forms.ToolStripLabel()
+        Me.wb1 = New System.Windows.Forms.WebBrowser()
+        Me.tm_ref = New System.Windows.Forms.Timer(Me.components)
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.KopitiamToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CustomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cust_forumid = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton6 = New System.Windows.Forms.ToolStripButton()
+        Me.tm3 = New System.Windows.Forms.Timer(Me.components)
+        Me.ToolStripButton10 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton11 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.pst = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStrip1.SuspendLayout()
+        Me.ToolStrip2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton8, Me.ToolStripButton1, Me.ToolStripSeparator2, Me.ToolStripButton9, Me.ToolStripButton6, Me.ToolStripSeparator1, Me.tsp, Me.ToolStripButton7, Me.ToolStripButton5, Me.ToolStripButton4, Me.ToolStripButton3, Me.ToolStripButton10, Me.ToolStripButton2, Me.ToolStripButton11, Me.ToolStripSeparator3})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(954, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton8
+        '
+        Me.ToolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton8.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ForumMainPageToolStripMenuItem, Me.UserProfileToolStripMenuItem, Me.SeeUsersPostToolStripMenuItem, Me.SeeUsersTopicsToolStripMenuItem})
+        Me.ToolStripButton8.Image = CType(resources.GetObject("ToolStripButton8.Image"), System.Drawing.Image)
+        Me.ToolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton8.Name = "ToolStripButton8"
+        Me.ToolStripButton8.Size = New System.Drawing.Size(41, 22)
+        Me.ToolStripButton8.Text = "LYN"
+        '
+        'ForumMainPageToolStripMenuItem
+        '
+        Me.ForumMainPageToolStripMenuItem.Name = "ForumMainPageToolStripMenuItem"
+        Me.ForumMainPageToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.ForumMainPageToolStripMenuItem.Text = "Forum Main Page"
+        '
+        'UserProfileToolStripMenuItem
+        '
+        Me.UserProfileToolStripMenuItem.Name = "UserProfileToolStripMenuItem"
+        Me.UserProfileToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.UserProfileToolStripMenuItem.Text = "User Profile"
+        '
+        'SeeUsersPostToolStripMenuItem
+        '
+        Me.SeeUsersPostToolStripMenuItem.Name = "SeeUsersPostToolStripMenuItem"
+        Me.SeeUsersPostToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.SeeUsersPostToolStripMenuItem.Text = "See User's Post"
+        '
+        'SeeUsersTopicsToolStripMenuItem
+        '
+        Me.SeeUsersTopicsToolStripMenuItem.Name = "SeeUsersTopicsToolStripMenuItem"
+        Me.SeeUsersTopicsToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.SeeUsersTopicsToolStripMenuItem.Text = "See User's Topics"
+        '
+        'ToolStripButton9
+        '
+        Me.ToolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton9.Image = CType(resources.GetObject("ToolStripButton9.Image"), System.Drawing.Image)
+        Me.ToolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton9.Name = "ToolStripButton9"
+        Me.ToolStripButton9.Size = New System.Drawing.Size(46, 22)
+        Me.ToolStripButton9.Text = "Scrape"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(71, 22)
+        Me.ToolStripButton2.Text = "Edit Thread"
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton3.Image = CType(resources.GetObject("ToolStripButton3.Image"), System.Drawing.Image)
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(101, 22)
+        Me.ToolStripButton3.Text = "Title and content"
+        '
+        'ToolStripButton4
+        '
+        Me.ToolStripButton4.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton4.Image = CType(resources.GetObject("ToolStripButton4.Image"), System.Drawing.Image)
+        Me.ToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton4.Name = "ToolStripButton4"
+        Me.ToolStripButton4.Size = New System.Drawing.Size(98, 22)
+        Me.ToolStripButton4.Text = "Submit Changes"
+        '
+        'ToolStripButton5
+        '
+        Me.ToolStripButton5.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton5.Image = CType(resources.GetObject("ToolStripButton5.Image"), System.Drawing.Image)
+        Me.ToolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton5.Name = "ToolStripButton5"
+        Me.ToolStripButton5.Size = New System.Drawing.Size(72, 22)
+        Me.ToolStripButton5.Text = "Close Topic"
+        '
+        'tsp
+        '
+        Me.tsp.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.tsp.Name = "tsp"
+        Me.tsp.Size = New System.Drawing.Size(100, 22)
+        '
+        'ToolStripButton7
+        '
+        Me.ToolStripButton7.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton7.Image = CType(resources.GetObject("ToolStripButton7.Image"), System.Drawing.Image)
+        Me.ToolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton7.Name = "ToolStripButton7"
+        Me.ToolStripButton7.Size = New System.Drawing.Size(43, 22)
+        Me.ToolStripButton7.Text = "Do All"
+        '
+        'tm
+        '
+        Me.tm.Interval = 5000
+        '
+        'tm2
+        '
+        Me.tm2.Interval = 7000
+        '
+        'lb1
+        '
+        Me.lb1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lb1.FormattingEnabled = True
+        Me.lb1.Location = New System.Drawing.Point(0, 25)
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(954, 69)
+        Me.lb1.TabIndex = 1
+        '
+        'ToolStrip2
+        '
+        Me.ToolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.stat, Me.l_ref, Me.ToolStripSeparator4, Me.pst})
+        Me.ToolStrip2.Location = New System.Drawing.Point(0, 489)
+        Me.ToolStrip2.Name = "ToolStrip2"
+        Me.ToolStrip2.Size = New System.Drawing.Size(954, 25)
+        Me.ToolStrip2.TabIndex = 2
+        Me.ToolStrip2.Text = "ToolStrip2"
+        '
+        'stat
+        '
+        Me.stat.Name = "stat"
+        Me.stat.Size = New System.Drawing.Size(73, 22)
+        Me.stat.Text = "Please Login"
+        '
+        'l_ref
+        '
+        Me.l_ref.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.l_ref.Name = "l_ref"
+        Me.l_ref.Size = New System.Drawing.Size(76, 22)
+        Me.l_ref.Text = "Refreshing in"
+        '
+        'wb1
+        '
+        Me.wb1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wb1.Location = New System.Drawing.Point(0, 94)
+        Me.wb1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wb1.Name = "wb1"
+        Me.wb1.ScriptErrorsSuppressed = True
+        Me.wb1.Size = New System.Drawing.Size(954, 395)
+        Me.wb1.TabIndex = 4
+        '
+        'tm_ref
+        '
+        Me.tm_ref.Enabled = True
+        Me.tm_ref.Interval = 1000
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KopitiamToolStripMenuItem, Me.AllToolStripMenuItem, Me.CustomToolStripMenuItem})
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(55, 22)
+        Me.ToolStripButton1.Text = "Forum"
+        '
+        'KopitiamToolStripMenuItem
+        '
+        Me.KopitiamToolStripMenuItem.Name = "KopitiamToolStripMenuItem"
+        Me.KopitiamToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.KopitiamToolStripMenuItem.Text = "Kopitiam"
+        '
+        'AllToolStripMenuItem
+        '
+        Me.AllToolStripMenuItem.Name = "AllToolStripMenuItem"
+        Me.AllToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.AllToolStripMenuItem.Text = "All"
+        '
+        'CustomToolStripMenuItem
+        '
+        Me.CustomToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.cust_forumid})
+        Me.CustomToolStripMenuItem.Name = "CustomToolStripMenuItem"
+        Me.CustomToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.CustomToolStripMenuItem.Text = "Custom Forum ID"
+        '
+        'cust_forumid
+        '
+        Me.cust_forumid.Name = "cust_forumid"
+        Me.cust_forumid.Size = New System.Drawing.Size(100, 23)
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton6
+        '
+        Me.ToolStripButton6.CheckOnClick = True
+        Me.ToolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton6.Image = CType(resources.GetObject("ToolStripButton6.Image"), System.Drawing.Image)
+        Me.ToolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton6.Name = "ToolStripButton6"
+        Me.ToolStripButton6.Size = New System.Drawing.Size(75, 22)
+        Me.ToolStripButton6.Text = "Auto Scrape"
+        '
+        'tm3
+        '
+        Me.tm3.Interval = 3000
+        '
+        'ToolStripButton10
+        '
+        Me.ToolStripButton10.CheckOnClick = True
+        Me.ToolStripButton10.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton10.Image = CType(resources.GetObject("ToolStripButton10.Image"), System.Drawing.Image)
+        Me.ToolStripButton10.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton10.Name = "ToolStripButton10"
+        Me.ToolStripButton10.Size = New System.Drawing.Size(45, 22)
+        Me.ToolStripButton10.Text = "Purge!"
+        '
+        'ToolStripButton11
+        '
+        Me.ToolStripButton11.CheckOnClick = True
+        Me.ToolStripButton11.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton11.Image = CType(resources.GetObject("ToolStripButton11.Image"), System.Drawing.Image)
+        Me.ToolStripButton11.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton11.Name = "ToolStripButton11"
+        Me.ToolStripButton11.Size = New System.Drawing.Size(38, 22)
+        Me.ToolStripButton11.Text = "Stop!"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(6, 25)
+        '
+        'pst
+        '
+        Me.pst.Name = "pst"
+        Me.pst.Size = New System.Drawing.Size(26, 22)
+        Me.pst.Text = "Idle"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(954, 514)
+        Me.Controls.Add(Me.wb1)
+        Me.Controls.Add(Me.ToolStrip2)
+        Me.Controls.Add(Me.lb1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LYN Forum User Management Script"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ToolStrip2.ResumeLayout(False)
+        Me.ToolStrip2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents ToolStripButton4 As ToolStripButton
+    Friend WithEvents ToolStripButton5 As ToolStripButton
+    Friend WithEvents tsp As ToolStripProgressBar
+    Friend WithEvents tm As Timer
+    Friend WithEvents tm2 As Timer
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripButton7 As ToolStripButton
+    Friend WithEvents ToolStripButton9 As ToolStripButton
+    Friend WithEvents lb1 As ListBox
+    Friend WithEvents ToolStrip2 As ToolStrip
+    Friend WithEvents stat As ToolStripLabel
+    Friend WithEvents l_ref As ToolStripLabel
+    Friend WithEvents wb1 As WebBrowser
+    Friend WithEvents tm_ref As Timer
+    Friend WithEvents ToolStripButton8 As ToolStripDropDownButton
+    Friend WithEvents ForumMainPageToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UserProfileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SeeUsersPostToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SeeUsersTopicsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KopitiamToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AllToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CustomToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents cust_forumid As ToolStripTextBox
+    Friend WithEvents ToolStripButton1 As ToolStripDropDownButton
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents ToolStripButton6 As ToolStripButton
+    Friend WithEvents tm3 As Timer
+    Friend WithEvents ToolStripButton10 As ToolStripButton
+    Friend WithEvents ToolStripButton11 As ToolStripButton
+    Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator4 As ToolStripSeparator
+    Friend WithEvents pst As ToolStripLabel
+End Class
---
+++ mis2100cheatapp/Form1.vb
@@ -0,0 +1,198 @@
+﻿Public Class Form1
+    Public isl As Boolean = False
+    Dim stepee As Integer = 0
+    Dim ref_i As Integer = 5
+    Dim usid As String
+    Dim forumc As String
+    Sub ck(ByVal z As String)
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("value") = z Then
+                ele.InvokeMember("click")
+            End If
+        Next
+    End Sub
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        wb1.Navigate(lb1.SelectedItem.replace("act=ST&f", "act=Post&CODE=08&f"))
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs) Handles ToolStripButton4.Click
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("value") = "Submit Modified Post" Then
+                ele.InvokeMember("click")
+            End If
+        Next
+    End Sub
+
+    Private Sub ToolStripButton5_Click(sender As Object, e As EventArgs) Handles ToolStripButton5.Click
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("value") = "Go" Then
+                ele.InvokeMember("click")
+            End If
+        Next
+    End Sub
+
+    Private Sub wb1_ProgressChanged(sender As Object, e As WebBrowserProgressChangedEventArgs)
+        On Error Resume Next
+        tsp.Maximum = e.MaximumProgress
+        tsp.Value = e.CurrentProgress
+    End Sub
+
+    Private Sub tm_Tick(sender As Object, e As EventArgs) Handles tm.Tick
+        If stepee = 1 Then ToolStripButton3.PerformClick()
+        If stepee = 2 Then
+            ToolStripButton3.PerformClick()
+        End If
+        If stepee = 3 Then
+            ToolStripButton3.PerformClick()
+            ToolStripButton4.PerformClick()
+        End If
+        If stepee = 4 Then
+            ToolStripButton4.PerformClick()
+            If Not lb1.SelectedIndex = lb1.Items.Count - 1 Then
+                lb1.SelectedIndex += 1
+            Else
+                tm.Enabled = False
+                Exit Sub
+            End If
+        End If
+        tm.Enabled = False
+        ToolStripButton7.PerformClick()
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("name") = "TopicTitle" Then
+                ele.InnerText = "------"
+            End If
+            If ele.GetAttribute("name") = "TopicDesc" Then
+                ele.InnerText = "------"
+            End If
+            If ele.GetAttribute("name") = "Post" Then
+                ele.InnerText = "This post has been overwritten by a script."
+            End If
+        Next
+    End Sub
+
+
+    Private Sub ToolStripButton7_Click_1(sender As Object, e As EventArgs) Handles ToolStripButton7.Click
+        If Not stepee >= 4 Then
+            stepee += 1
+        Else
+            stepee = 1
+        End If
+        tm.Enabled = True
+    End Sub
+    Private Sub ToolStripButton9_Click(sender As Object, e As EventArgs) Handles ToolStripButton9.Click
+        For Each ClientControl As HtmlElement In wb1.Document.Links
+            If ClientControl.GetAttribute("href").Contains("https://forum.lowyat.net/index.php?act=ST&f=" & forumc) Then
+                lb1.Items.Add(ClientControl.GetAttribute("href").Replace("act=ST&f", "act=Post&CODE=08&f"))
+            End If
+        Next
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("title") = "Next page" Then
+                ele.InvokeMember("click")
+            End If
+        Next
+    End Sub
+
+    Private Sub lb1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lb1.SelectedIndexChanged
+        tm.Enabled = False
+        wb1.Navigate(lb1.SelectedItem)
+        stepee = 0
+        tm2.Enabled = True
+        tsp.Maximum = lb1.Items.Count - 1
+        tsp.Value = lb1.SelectedIndex
+    End Sub
+
+    Private Sub tm2_Tick(sender As Object, e As EventArgs) Handles tm2.Tick
+        ToolStripButton7.PerformClick()
+        tm2.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        wb1.Navigate("https://forum.lowyat.net/index.php?act=Login&CODE=00")
+    End Sub
+
+    Private Sub tm_ref_Tick(sender As Object, e As EventArgs) Handles tm_ref.Tick
+        If ref_i = 0 Then
+            l_ref.PerformClick()
+        Else
+            ref_i -= 1
+            l_ref.Text = "Refreshing in " & ref_i & " seconds"
+        End If
+    End Sub
+
+    Private Sub l_ref_Click(sender As Object, e As EventArgs) Handles l_ref.Click
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("href").Contains("https://forum.lowyat.net/index.php?act=Login&CODE=03&id=") Then
+                usid = ele.GetAttribute("href").Replace("https://forum.lowyat.net/index.php?act=Login&CODE=03&id=", "")
+                stat.Text = "User ID: " & usid
+            End If
+            If ele.GetAttribute("id") = "userlinks" Then
+                tm_ref.Enabled = False
+                l_ref.Text = "Manual Refresh"
+            Else
+                ref_i = 5
+            End If
+        Next
+    End Sub
+
+    Private Sub ForumMainPageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ForumMainPageToolStripMenuItem.Click
+        wb1.Navigate("http://forum.lowyat.net")
+    End Sub
+
+    Private Sub SeeUsersPostToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SeeUsersPostToolStripMenuItem.Click
+        wb1.Navigate("https://forum.lowyat.net/index.php?act=Search&CODE=getalluser&mid=" & usid)
+
+    End Sub
+
+    Private Sub SeeUsersTopicsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SeeUsersTopicsToolStripMenuItem.Click
+        wb1.Navigate("https://forum.lowyat.net/index.php?act=Search&CODE=gettopicsuser&mid=" & usid)
+    End Sub
+
+    Private Sub UserProfileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UserProfileToolStripMenuItem.Click
+        wb1.Navigate("https://forum.lowyat.net/index.php?showuser=" & usid)
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+
+    End Sub
+
+    Private Sub cust_forumid_TextChanged(sender As Object, e As EventArgs) Handles cust_forumid.TextChanged
+        forumc = cust_forumid.Text
+    End Sub
+
+    Private Sub KopitiamToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KopitiamToolStripMenuItem.Click
+        forumc = "23"
+    End Sub
+
+    Private Sub AllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AllToolStripMenuItem.Click
+        forumc = ""
+    End Sub
+
+    Private Sub tm3_Tick(sender As Object, e As EventArgs) Handles tm3.Tick
+        For Each ele As HtmlElement In wb1.Document.All
+            If ele.GetAttribute("title") = "Next page" Then
+                ToolStripButton9.PerformClick()
+                Exit Sub
+            End If
+        Next
+        ToolStripButton6.Checked = False
+        tm3.Enabled = False
+    End Sub
+
+    Private Sub ToolStripButton6_Click(sender As Object, e As EventArgs) Handles ToolStripButton6.Click
+        tm3.Enabled = ToolStripButton6.Checked
+    End Sub
+
+    Private Sub ToolStripButton10_Click(sender As Object, e As EventArgs) Handles ToolStripButton10.Click
+        lb1.SelectedIndex = 0
+        pst.Text = "Purge in progress..."
+    End Sub
+
+    Private Sub ToolStripButton11_Click(sender As Object, e As EventArgs) Handles ToolStripButton11.Click
+        tm.Enabled = False
+        tm2.Enabled = False
+        pst.Text = "On hold"
+    End Sub
+End Class
